0bbfc5c5beb0456d3e834493f05bf6e4628e4432
Adam Stachowiak <adam.a.stach@gmail.com>
2023-07-11 21:57:22 +0200

    Style modelling part 2

--- src/App.tsx
+++ src/App.tsx
@@ -25,39 +25,48 @@ function App() {
           <img src="src/images/tetris.png" alt="tetris" />
         </header>
       </div>
-      <Tetris className="pixel"></Tetris>
-      <div id="sidebar">
-        <div id="score">
-          <span>PUNKTY</span>
-        </div>
-        <div id="next-block"></div>
-        <div id="buttons">
-          <div id="up-button">
-            <img src="src/images/up.png"></img>
+      <div id="rest">
+        <Tetris className="pixel"></Tetris>
+        <div id="sidebar">
+          <div id="score">
+            <span>PUNKTY</span>
+            <span>100</span>
           </div>
-          <div id="middle-buttons">
-            {click != 1 ? (
-              <img src="src/images/left.png"></img>
+          <div id="next">
+            <span>NASTĘPNY BLOK</span>
+            <div id="next-block"></div>
+          </div>
+          <div id="buttons">
+            <div id="up-button">
+              <img src="src/images/up.png"></img>
+            </div>
+            <div id="middle-buttons">
+              {click != 1 ? (
+                <img src="src/images/left.png"></img>
+              ) : (
+                <img src="src/images/left.png" className="shadow"></img>
+              )}
+              <img src="src/images/push.png"></img>
+              {click != 2 ? (
+                <img src="src/images/right.png"></img>
+              ) : (
+                <img src="src/images/right.png" className="shadow"></img>
+              )}
+            </div>
+            <div id="bottom-button">
+              <img src="src/images/down.png"></img>
+            </div>
+          </div>
+          <div id="turn-button">
+            {click != 3 ? (
+              <img src="src/images/turn_right.png"></img>
             ) : (
-              <img src="src/images/left.png" className="shadow"></img>
-            )}
-            <img src="src/images/push.png"></img>
-            {click != 2 ? (
-              <img src="src/images/right.png"></img>
-            ) : (
-              <img src="src/images/right.png" className="shadow"></img>
+              <img src="src/images/turn_right.png" className="shadow"></img>
             )}
           </div>
-          <div id="bottom-button">
-            <img src="src/images/down.png"></img>
-          </div>
-        </div>
-        <div id="turn-button">
-          {click != 3 ? (
-            <img src="src/images/turn_right.png"></img>
-          ) : (
-            <img src="src/images/turn_right.png" className="shadow"></img>
-          )}
+          <button id="start" className="btn btn-light">
+            START
+          </button>
         </div>
       </div>
     </div>
